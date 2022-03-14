b552f32584eb680370ec645798530aed6aee6e55
Aleksei DCM <some@mail.com>
2022-03-14 21:53:10 +0300

    added active onclick

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ let header_menu = document.querySelector('.header_menu');
 let back = document.querySelector('body');
 let header_list = document.querySelector('.header_list');
 
-header_burger.onclick = function(){     // actvie при событии onclick
+// actvie при onclick
+
+header_burger.onclick = function(){ 
     header_burger.classList.toggle('active');
     header_menu.classList.toggle('active');
     back.classList.toggle('lock');
